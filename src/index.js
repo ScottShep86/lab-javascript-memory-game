ca08836730f359b1dfbf6f26acec3344fa901408
Scott Shepherd <scottshep86@gmail.com>
2023-04-13 16:57:54 +0200

    final game completed

--- src/index.js
+++ src/index.js
@@ -26,9 +26,19 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+let pairsClickedElement = document.querySelector("#pairs-clicked");
+let pairsGuessedElement = document.querySelector("#pairs-guessed");
+let gameoverScreen = document.querySelector("#gameover-container");
+let movesElement = document.querySelector("#moves");
+let restartBtn = document.querySelector("#restartBtn");
+let waiting = false;
+let pairs = [];
 
-window.addEventListener('load', (event) => {
-  let html = '';
+window.addEventListener("load", (event) => {
+  gameoverScreen.style.display = "none";
+  memoryGame.shuffleCards(cards);
+
+  let html = "";
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -39,13 +49,115 @@ window.addEventListener('load', (event) => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
-
+  document.querySelector("#memory-board").innerHTML = html;
   // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      let cardName = card.getAttribute("data-card-name");
+      pairs.push(cardName);
+      if (waiting === false) {
+        card.classList.add("turned");
+        //when two cards have been flipped over, check if they match
+        if (pairs.length === 2) {
+          waiting = true;
+          //use method from memory.js to check the names of the cards
+          let match = memoryGame.checkIfPair(pairs[0], pairs[1]);
+          //update the DOM for the cards clicked
+          pairsClickedElement.innerText = memoryGame.pairsClicked;
+          //update the DOM to reflect the pairs correctly matched
+          pairsGuessedElement.innerText = memoryGame.pairsGuessed;
+
+          if (match === false) {
+            setTimeout(() => {
+              document.querySelectorAll(".card").forEach((card) => {
+                if (pairs.includes(card.getAttribute("data-card-name"))) {
+                  card.classList.remove("turned");
+                  waiting = false;
+                }
+              });
+              pairs = [];
+            }, 2000);
+          } else {
+            waiting = false;
+            pairs = [];
+          }
+        }
+        let gameFinished = memoryGame.checkIfFinished();
+        if (gameFinished) {
+          movesElement.innerText = memoryGame.pairsClicked;
+          gameoverScreen.style.display = "flex";
+        }
+      }
+    });
+  });
+
+  restartBtn.addEventListener("click", () => {
+    memoryGame.pairsClicked = 0;
+    memoryGame.pairsGuessed = 0;
+    pairs = [];
+    //update the DOM for the cards clicked
+    pairsClickedElement.innerText = memoryGame.pairsClicked;
+    //update the DOM to reflect the pairs correctly matched
+    pairsGuessedElement.innerText = memoryGame.pairsGuessed;
+    gameoverScreen.style.display = "none";
+    memoryGame.shuffleCards(cards);
+
+    let html = "";
+    memoryGame.cards.forEach((pic) => {
+      html += `
+      <div class="card" data-card-name="${pic.name}">
+        <div class="back" name="${pic.img}"></div>
+        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+      </div>
+    `;
+    });
+
+    // Add all the divs to the HTML
+    document.querySelector("#memory-board").innerHTML = html;
+    // Bind the click event of each element to a function
+    document.querySelectorAll(".card").forEach((card) => {
+      card.classList.remove("turned");
+    });
+
+    //start over
+    document.querySelectorAll(".card").forEach((card) => {
+      card.addEventListener("click", () => {
+        let cardName = card.getAttribute("data-card-name");
+        pairs.push(cardName);
+        if (waiting === false) {
+          card.classList.add("turned");
+          //when two cards have been flipped over, check if they match
+          if (pairs.length === 2) {
+            waiting = true;
+            //use method from memory.js to check the names of the cards
+            let match = memoryGame.checkIfPair(pairs[0], pairs[1]);
+            //update the DOM for the cards clicked
+            pairsClickedElement.innerText = memoryGame.pairsClicked;
+            //update the DOM to reflect the pairs correctly matched
+            pairsGuessedElement.innerText = memoryGame.pairsGuessed;
+
+            if (match === false) {
+              setTimeout(() => {
+                document.querySelectorAll(".card").forEach((card) => {
+                  if (pairs.includes(card.getAttribute("data-card-name"))) {
+                    card.classList.remove("turned");
+                    waiting = false;
+                  }
+                });
+                pairs = [];
+              }, 2000);
+            } else {
+              waiting = false;
+              pairs = [];
+            }
+          }
+          let gameFinished = memoryGame.checkIfFinished();
+          if (gameFinished) {
+            movesElement.innerText = memoryGame.pairsClicked;
+            gameoverScreen.style.display = "flex";
+          }
+        }
+      });
     });
   });
 });
--- src/memory.js
+++ src/memory.js
@@ -2,17 +2,54 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
     // ... write your code here
-  }
+    if (this.cards)
+      // Loop through the array starting from the last index:
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        // Save a copy of the item at the position you are currently looping:
+        const copyOfItemAtPositionBeingLooped = this.cards[i];
+    
+        // Select a random index position:
+        const randomlySelectedPosition = Math.floor(Math.random() * (i + 1));
+    
+        // === The swapping takes place in the two statements below ===
+        // Replace the item at the position you are currently looping with the one at the randomlySelectedPosition:
+        this.cards[i] = this.cards[randomlySelectedPosition];
+    
+        // Replace the item at the randomlySelectedPosition with the copyOfItemAtPositionBeingLooped:
+        this.cards[randomlySelectedPosition] = copyOfItemAtPositionBeingLooped;
+      }
+    
+      // Output the shuffled array:
+      return this.cards;
+    }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked +=1; 
+
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      this.pairsGuessed;
+      return false;
+    };
   }
 
   checkIfFinished() {
     // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+    return false;
+    };
+
   }
 }
